f96dfb3ed4382900860ab034ecd6a3f5a1463126
Shunting <zhangst1986@gmail.com>
2012-04-30 22:17:45 +0800

    support saving http headers

--- Main.php
+++ Main.php
@@ -6,12 +6,14 @@ class Main {
   private $appId = '264209280313575';
   private $appSecret = 'f52ed5d2409780bbaf02cedfae6a8542';
   private $siteUrl = 'http://localhost:3080';
-private $userAgent = 'Mozilla/5.0 (X11; Linux x86_64; rv:2.0) Gecko/20100101 Firefox/4.0';
+  private $userAgent = 'Mozilla/5.0 (X11; Linux x86_64; rv:2.0) Gecko/20100101 Firefox/4.0';
+
+  public static $headerLoc = "temp/curl.httpheaders";
 
   // example: https://www.facebook.com/dialog/oauth?client_id=264209280313575&redirect_uri=http://localhost:3080/ignores/show-request.php&scope=email&response_type=token
   // need extra options to access this url because of server side browser detection and redirection.
   // A workable url is:
-  //   curl -H 'Accept-Language: en-us,en;q=0.5' -L -A 'Mozilla/5.0 (X11; Linux x86_64; rv:2.0) Gecko/20100101 Firefox/4.0' <this url>
+  //   curl -k -H 'Accept-Language: en-us,en;q=0.5' -L -A 'Mozilla/5.0 (X11; Linux x86_64; rv:2.0) Gecko/20100101 Firefox/4.0' <this url>
   public function genClientTokenFetchUrl($appId, $redirectUrl, $scopes) {
     $baseUrl = 'https://www.facebook.com/dialog/oauth';
 	$scopeStr = implode(',', $scopes);
@@ -28,12 +30,15 @@ private $userAgent = 'Mozilla/5.0 (X11; Linux x86_64; rv:2.0) Gecko/20100101 Fir
     $handle = curl_init($url); 
 	$handle !== false or die('Fail to invoke curl_init');
 
+    $fh = fopen(self::$headerLoc, "w");
+	$fh !== false or die('Fail to open the file to store http headers');
 	$options = array(
 	  CURLOPT_RETURNTRANSFER => true,
 	  CURLOPT_FOLLOWLOCATION => true,
 	  CURLOPT_USERAGENT => $this->userAgent,
 	  CURLOPT_FOLLOWLOCATION => true,
 	  CURLOPT_SSL_VERIFYPEER => false,
+	  CURLOPT_WRITEHEADER => $fh,
 	  CURLOPT_HTTPHEADER => array(
 	    'Accept-Language: en-us,en;q=0.5', // solve the chinese problem
 	  ),
@@ -42,7 +47,7 @@ private $userAgent = 'Mozilla/5.0 (X11; Linux x86_64; rv:2.0) Gecko/20100101 Fir
 	$response = curl_exec($handle);
 	$response !== false or die('Fail to invoke curl_exec, error message is ' . curl_error($handle) . "\n");
 	curl_close($handle);
-
+	fclose($fh) or die('Fail to close the file that stores http headers');
 	return $response;
   }
 
